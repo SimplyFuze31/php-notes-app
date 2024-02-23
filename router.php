f91341a5630456f5bf3d78fb4f0c65299977387f
SimplyFuze31 <kondratuiksasha5@gmail.com>
2024-02-23 14:14:45 +0200

    added controller and view for the main page

--- router.php
+++ router.php
@@ -1,1 +1,17 @@
 <?php
+
+$routes = require 'routes.php';
+
+
+
+function routeToController($url,$routes)
+{
+    if(array_key_exists($url,$routes)){
+        require $routes[$url];
+    }else{
+        abort();
+    }
+
+}
+
+routeToController($_SERVER['REQUEST_URI'],$routes);
