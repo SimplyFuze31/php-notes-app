b6df3ecdae92ef9d7973523653ad9dbd52f99b00
SimplyFuze31 <kondratuiksasha5@gmail.com>
2024-03-20 13:19:38 +0200

    Add ability to update notes

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,19 +1,36 @@
 <?php
 
 use Core\Database;
+use Core\Validator;
 
 $currentUser = 1;
 
+$errors = [];
 $config = require base_path('config.php');
 $db = new Database($config['database'], 'root', 'root');
+$note = $db->query('SELECT * FROM notes where id = :id',['id' => $_GET['id']])->getOrFail();
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-    header('Location: http://notes.nl/notes');
-    die();
+
+    if(!Validator::string($_POST['title'], max: 300))
+        $errors['title'] = 'The title field should be from 1 to 300 characters';
+    if (!Validator::string($_POST['note_text']))
+        $errors['note_text'] = 'The text field should be from 1 to 1500 characters';
+
+    if (count($errors) === 0 ){
+        $db->query('update notes set title = :title, note_text = :note_text where id=:id; ',[
+            'id' => $note['id'],
+            'title' => $_POST['title'],
+            'note_text'=>$_POST['note_text'],
+        ]);
+        header('Location: http://notes.nl/notes');
+        die();
+    }
 }
 
 
-$note = $db->query('SELECT * FROM notes where id = :id',['id' => $_GET['id']])->getOrFail();
 
 authorize($note['user_id'] === $currentUser);
 $heading = 'Save';
